822f134d3a2a9ff229592dc56e28603eed72552f
nitoyon <nitoyon@gmail.com>
2012-06-24 18:39:47 +0900

    Modified convert_hatena_to_jekyll_posts.rb.
    ・本文に {% hoge %} や {{ hoge }} が入っていたときに Liquid の解釈で
      エラーがでる問題を修正した。
    ・twitter 記法 [twitter:123456789:detail] を tweet プラグインに変換
      するようにした。
    ・サイト内リンクの [http://d.hatena.ne.jp/id/...:title] を
      post_link プラグインで表示するように修正した。

--- _scripts/convert_hatena_to_jekyll_posts.rb
+++ _scripts/convert_hatena_to_jekyll_posts.rb
@@ -24,20 +24,26 @@ hatena_id = ARGV[0]
 xml_path = ARGV[1]
 puts "\nhatena id: #{hatena_id}\nXML path: #{xml_path}\n\n"
 
-# XML を開く
-file = File.new(xml_path)
-doc = REXML::Document.new file
+def main(hatena_id, xml_path)
+  # XML を開く
+  file = File.new(xml_path)
+  doc = REXML::Document.new file
 
-# 各日ごとに処理
-# <diary>
-#   <day date="YYYY-MM-DD" title="">
-#     <body>...</body>
-#   </day>
+  # 各日ごとに処理
+  # <diary>
+  #   <day date="YYYY-MM-DD" title="">
+  #     <body>...</body>
+  #   </day>
 # </diary>
-doc.elements.each("/diary/day") { |e|
+  doc.elements.each("/diary/day") { |e|
+    parse_day(e, hatena_id)
+  }
+end
+
+def parse_day(e, hatena_id)
   date = e.attributes["date"]
   body = e.elements["body"].text
-  if date < "2006-01-01" then next end
+  if date < "2006-01-01" then return end
 
   # エントリーごとに処理
   # body には複数エントリーが含まれることがある
@@ -77,9 +83,35 @@ doc.elements.each("/diary/day") { |e|
         "lang" => "ja",
         "old_url" => "http://d.hatena.ne.jp/#{hatena_id}/#{old_url}",
       }.to_yaml)
-      f.write("\n---\n")
-      f.write(text)
+      f.write("---\n")
+      f.write(convert_text(text, hatena_id))
     }
     puts "Success #{fn}"
   }
-}
+end
+
+def convert_text(text, hatena_id)
+    # 本文の {% hoge %} や {{ hoge }}が Liquid テンプレートタグとして解釈
+    # されないようにしておく
+    tokens = text.split(/(#{Liquid::PartialTemplateParser})/)
+    tokens.shift if tokens[0] and tokens[0].empty?
+    tokens = tokens.map {|t|
+      t = "{%raw%}#{t}{%endraw%}" if t.match(Liquid::AnyStartingTag)
+      t
+    }
+    text = tokens.join("")
+
+    # [twitter:123456789:detail] → {% twitter 123456789 %}
+    text = text.gsub(/\[twitter:(\d+):detail\]/, '{% twitter \1 %}')
+
+    # [http://d.hatena.ne.jp/#{hatena_id}/YYYYMMDD/name:title]
+    # → {% post_link YYYY-MM-DD-name %}
+    text = text.gsub(/\[http:\/\/d\.hatena\.ne\.jp\/#{hatena_id}\/(\d{4})(\d{2})(\d{2})\/([^\]]+):title\]/) { |text|
+        "{% post_link #{$1}-#{$2}-#{$3}-#{$4.gsub('_', '-')} %}"
+    }
+
+    text
+end
+
+
+main(hatena_id, xml_path)
