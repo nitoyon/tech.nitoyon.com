15d2ec7efe08d6d37ce07aa5e970f63b73546bac
nitoyon <nitoyon@gmail.com>
2012-06-30 18:58:22 +0900

    Fixed archives.monthes enumeration in posts.js

--- _lang/posts.js
+++ _lang/posts.js
@@ -13,7 +13,7 @@ Site.archives.posts = [
 ];
 
 Site.archives.monthes = [
-{% for post in site.posts %}{% capture this_year %}{{ post.date | date: "%Y" }}{% endcapture %}{% capture this_month %}{{ post.date | date: "%m" }}{% endcapture %}{% capture next_year %}{{ post.previous.date | date: "%Y" }}{% endcapture %}{% capture next_month %}{{ post.previous.date | date: "%m" }}{% endcapture %}{% if forloop.first %}  "{{this_year}}{{this_month}}"
+{% for post in posts %}{% capture this_year %}{{ post.date | date: "%Y" }}{% endcapture %}{% capture this_month %}{{ post.date | date: "%m" }}{% endcapture %}{% capture next_year %}{{ post.previous.date | date: "%Y" }}{% endcapture %}{% capture next_month %}{{ post.previous.date | date: "%m" }}{% endcapture %}{% if forloop.first %}  "{{this_year}}{{this_month}}"
 {% elsif forloop.last %}{% elsif this_year != next_year %}, "{{this_year}}{{this_month}}"
 {% elsif this_month != next_month %}, "{{this_year}}{{this_month}}"
 {% endif %}{% endfor %}];
