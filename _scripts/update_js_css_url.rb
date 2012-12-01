66c03f4f605d51ae5b79d40bd6a9b47d46155a3e
nitoyon <nitoyon@gmail.com>
2012-12-01 14:53:08 +0900

    Add '-v' commandline option

--- _scripts/update_js_css_url.rb
+++ _scripts/update_js_css_url.rb
@@ -5,7 +5,7 @@
 #
 # This script modifies JavaScript and CSS URL in all HTML files.
 #
-# USAGE: update_js_css_url.rb [-d] site_dir [target_dir]
+# USAGE: update_js_css_url.rb [-d] [-v] site_dir [target_dir]
 # 
 # (example)
 #     $ ruby update_js_css_url.rb path/to/site/dir
@@ -20,18 +20,34 @@
 #
 # Added value (`1234567890`) means timestamp of the file.
 
+$debug = false
+
+def log(str)
+  puts str if $debug
+end
+
 def main(site_dir, target_dir, delete_time)
   # Open file
-  puts target_dir
+  log "site_dir: #{site_dir}"
+  log "target_dir: #{target_dir}"
+
+  file_count = 0
+  text_count = 0
   Dir::glob("#{target_dir}/**/*.html").each { |f|
-    update_timestamp(site_dir, f, delete_time)
+    c = update_timestamp(site_dir, f, delete_time)
+    if c > 0
+      file_count += 1
+      text_count += c
+    end
   }
+
+  puts "replaced #{file_count} files (#{text_count} text)"
 end
 
 def update_timestamp(dir, file, delete_time)
   s = ""
   count = 0
-  puts "parsing #{file}..."
+  log "parsing #{file}..."
 
   open(file) { |f| s = f.read }
   new_mtime = File::mtime(file)
@@ -49,9 +65,11 @@ def update_timestamp(dir, file, delete_time)
   if s != s2
     open(file, 'w') { |f| f.write(s2) }
     File::utime(new_mtime, new_mtime, file)
-    puts "wrote #{file} (replaced #{count})"
+    log "wrote #{file} (replaced #{count})"
+    count
   else
-    puts "skipped #{file}"
+    log "skipped #{file}"
+    0
   end
 end
 
@@ -63,6 +81,10 @@ end
 delete_time = false
 if ARGV[0] == "-d"
   delete_time = true
+  ARGV.shift
+end
+if ARGV[0] == "-v"
+  $debug = true
   ARGV.shift
 end
 
