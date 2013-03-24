5b7462612df99b7c49e1fea8072898e03e9fa2a8
nitoyon <nitoyon@gmail.com>
2013-03-24 21:43:19 +0900

    Update post_link plugin: now we can specify category name

--- _plugins/tags/post_link.rb
+++ _plugins/tags/post_link.rb
@@ -4,11 +4,23 @@
 # How to use
 # ----------
 #
-#     {% post_link /2012/01/02/article-name.html %}
-#     => <a href="/2012/01/02/article-name.html">Article Title</a>
+# Embed with URL.
+#
+#     {% post_link /category/2012/01/02/article-name.html %}
+#     => <a href="/category/2012/01/02/article-name.html">Article Title</a>
+#
+# Embed with category name and file name.
+#
+#     {% post_link category/2012-01-02-article-name %}
+#     => <a href="/category/2012/01/02/article-name.html">Article Title</a>
+#
+# Embed with file name. (If there are multiple posts with same file name with
+# different category name, it is not undefined that which posts are selected.
 #
 #     {% post_link 2012-01-02-article-name %}
-#     => <a href="/2012/01/02/article-name.html">Article Title</a>
+#     => <a href="/category/2012/01/02/article-name.html">Article Title</a>
+#
+# Specify link text
 #
 #     {% post_link 2012-01-02-article-name, here %}
 #     => <a href="/2012/01/02/article-name.html">here</a>
@@ -23,6 +35,23 @@ require 'cgi'
 require 'jekyll/tags/post_url'
 
 module Jekyll
+
+  # PostCategoryComparer adds `categories` property to PostComparer
+  # (defined in lib/jekyll/tags/post_url.rb).
+  # Not that Post#<=>() only checks `YYYY-MM-DD-slug` are equal
+  class PostCategoryComparer
+    MATCHER = /^(.+\/)*(\d+-\d+-\d+)-(.*)$/
+
+    attr_accessor :date, :slug, :categories
+
+    def initialize(name)
+      m, dir, date, slug = *name.match(MATCHER)
+      dir ||= ""
+      @categories = dir.split('/').reject { |x| x.empty? }
+      @slug = slug
+      @date = Time.parse(date)
+    end
+  end
 
   class PostLinkTag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
@@ -47,18 +76,33 @@ module Jekyll
       end
 
       begin
-        post = PostComparer.new(url)
+        target_post = PostCategoryComparer.new(url)
       rescue
-        post = nil
+        target_post = nil
       end
 
+      # Find post
+      post = nil
       site.posts.each do |p|
-        if p.url == url || p == post
-          title = @title.nil? || @title.empty? ? p.data['title'] : @title
-          return %(<a href="#{p.url}">#{CGI.escapeHTML(title)}</a>)
+        if p.url == url
+          # stop enumerating if url exactly matches
+          post = p
+          break
+        elsif p == target_post
+          post = p
+          # stop enumerating if categories are specified and equal
+          if p.categories == target_post.categories
+            break
+          end
         end
       end
-      %(<a href="#{url}">#{CGI.escapeHTML(url)}</a>)
+
+      if post.nil?
+        %(<a href="#{url}">#{CGI.escapeHTML(url)}</a>)
+      else
+        title = @title.nil? || @title.empty? ? post.data['title'] : @title
+        %(<a href="#{post.url}">#{CGI.escapeHTML(title)}</a>)
+      end
     end
   end
 end
