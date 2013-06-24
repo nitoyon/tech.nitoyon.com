175c040d01884ec26b326d0480e7f5c80787a074
nitoyon <nitoyon@gmail.com>
2013-06-25 01:41:16 +0900

    Update for 2013-06-25

--- en/posts.js
+++ en/posts.js
@@ -12,15 +12,16 @@ var Site = {
 
 
 Site.archives.posts = [
+	{url: "/en/blog/2013/06/25/jekyll-grunt/", title: "Jekyll: Use Grunt instead of --watch option"},
 	{url: "/en/blog/2013/05/17/bugzilla-hack/", title: "Bugzilla: Edit bug status using Perl script"},
 	{url: "/en/blog/2013/04/17/jekyll-pluralize/", title: "3 ways to specify categories and tags in Jekyll"},
 	{url: "/en/blog/2013/03/25/livereloadx0-2-0/", title: "LiveReloadX 0.2.0 Released: try include/exclude filter"},
-	{url: "/en/blog/2013/02/27/livereloadx/", title: "LiveReloadX Released: Reload your Browser Automatically!"},
-	{url: "/en/blog/2009/04/05/irbweb-ruby-on-your-browser/", title: "IRBWEB - Try Ruby on Your Browser!"}
+	{url: "/en/blog/2013/02/27/livereloadx/", title: "LiveReloadX Released: Reload your Browser Automatically!"}
 ];
 
 Site.archives.months = [
-  "201305"
+  "201306"
+, "201305"
 , "201304"
 , "201303"
 , "201302"
--- ja/posts.js
+++ ja/posts.js
@@ -12,11 +12,11 @@ var Site = {
 
 
 Site.archives.posts = [
+	{url: "/ja/blog/2013/06/25/jekyll-grunt/", title: "Jekyll で --watch の代わりに Grunt を使ってみるテスト"},
 	{url: "/ja/blog/2013/06/17/blog-tag/", title: "ブログのタグ機能が復活したよ"},
 	{url: "/ja/blog/2013/05/17/bugzilla-hack/", title: "Bugzilla に登録してあるバグをプログラムから更新する方法"},
 	{url: "/ja/blog/2013/05/02/git-commit-amend/", title: "git commit --amend を省力化する方法"},
-	{url: "/ja/blog/2013/04/23/chrome-responsive-debug/", title: "Google Chrome で超手軽にスマホ向けデザインを確認する方法"},
-	{url: "/ja/blog/2013/04/17/jekyll-pluralize/", title: "Jekyll のカテゴリーとタグの指定方法 3 パターン"}
+	{url: "/ja/blog/2013/04/23/chrome-responsive-debug/", title: "Google Chrome で超手軽にスマホ向けデザインを確認する方法"}
 ];
 
 Site.archives.months = [
