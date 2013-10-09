ea77c66ac3c67a45748c8c8f9d0d104a79e31ef5
nitoyon <nitoyon@gmail.com>
2013-10-10 00:29:42 +0900

    Update for 2013-10-10

--- en/posts.js
+++ en/posts.js
@@ -8,11 +8,11 @@ var Site = {
 
 
 Site.archives.posts = [
+	{url: "/en/blog/2013/10/10/grunt-watch-slow/", title: "When grunt-contrib-watch uses too much CPU, try grunt-este-watch"},
 	{url: "/en/blog/2013/10/02/node-watch-impl/", title: "Difference between fs.watch() and fs.watchFile()"},
 	{url: "/en/blog/2013/07/09/symlink-dir-diff-on-windows/", title: "Use symlinks in git difftool --dir-diff on Windows"},
 	{url: "/en/blog/2013/06/25/jekyll-grunt/", title: "Jekyll: Use Grunt instead of --watch option"},
-	{url: "/en/blog/2013/05/17/bugzilla-hack/", title: "Bugzilla: Edit bug status using Perl script"},
-	{url: "/en/blog/2013/04/17/jekyll-pluralize/", title: "3 ways to specify categories and tags in Jekyll"}
+	{url: "/en/blog/2013/05/17/bugzilla-hack/", title: "Bugzilla: Edit bug status using Perl script"}
 ];
 
 Site.archives.months = [
