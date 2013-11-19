046772192b15ece562d21b9ef148103389584ef0
nitoyon <nitoyon@gmail.com>
2013-11-20 01:22:17 +0900

    Update for 2013-11-20

--- ja/posts.js
+++ ja/posts.js
@@ -12,11 +12,11 @@ var Site = {
 
 
 Site.archives.posts = [
+	{url: "/ja/blog/2013/11/20/cvs2git/", title: "CVS レポジトリを Git に変換した手順とか注意点とか"},
 	{url: "/ja/blog/2013/11/14/jekyll-win/", title: "Windows で Jekyll 1.3 を動かすまでの手順"},
 	{url: "/ja/blog/2013/11/07/k-means/", title: "K-means 法を D3.js でビジュアライズしてみた"},
 	{url: "/ja/blog/2013/10/29/d3js-svg-line/", title: "D3.js の d3.svg.line() を試してみた"},
-	{url: "/ja/blog/2013/10/24/d3js/", title: "D3.js の Data-Driven な DOM 操作がおもしろい"},
-	{url: "/ja/blog/2013/10/10/grunt-watch-slow/", title: "grunt-contrib-watch が重いので grunt-este-watch を試したら幸せになった"}
+	{url: "/ja/blog/2013/10/24/d3js/", title: "D3.js の Data-Driven な DOM 操作がおもしろい"}
 ];
 
 Site.archives.months = [
