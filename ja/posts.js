ce0c1a3f025fe84d403cca27d6e75117be9bf7d6
nitoyon <nitoyon@gmail.com>
2014-02-06 09:52:46 +0900

    Update for 2014-02-06

--- ja/posts.js
+++ ja/posts.js
@@ -12,15 +12,16 @@ var Site = {
 
 
 Site.archives.posts = [
+	{url: "/ja/blog/2014/02/06/rdp-port/", title: "リモート デスクトップでポート番号を指定して接続する方法"},
 	{url: "/ja/blog/2014/01/17/chef-win-path/", title: "Windows で Chef するときに PATH で混乱しないように専用のコンソールを作った"},
 	{url: "/ja/blog/2014/01/14/nenga/", title: "うちの年賀状2014"},
 	{url: "/ja/blog/2013/12/13/touch-viewer/", title: "タッチ操作に対応した画像ビューワーをJavaScriptで作るならD3.jsが便利"},
-	{url: "/ja/blog/2013/12/09/setctime/", title: "Windows でファイル作成日時をスクリプト言語から操作するために setctime.exe を作った"},
-	{url: "/ja/blog/2013/11/20/cvs2git/", title: "CVS レポジトリを Git に変換した手順とか注意点とか"}
+	{url: "/ja/blog/2013/12/09/setctime/", title: "Windows でファイル作成日時をスクリプト言語から操作するために setctime.exe を作った"}
 ];
 
 Site.archives.months = [
-  "201401"
+  "201402"
+, "201401"
 , "201312"
 , "201311"
 , "201310"
