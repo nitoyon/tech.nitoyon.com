6a4df12c148135520a2fc2fb057838ba6c0f75bf
nitoyon <nitoyon@gmail.com>
2014-02-26 02:03:05 +0900

    Update for 2014-02-26

--- en/posts.js
+++ en/posts.js
@@ -12,15 +12,16 @@ var Site = {
 
 
 Site.archives.posts = [
-	{url: "/en/blog/2014/01/29/chef-win-path/", title: "Chef Console: avoid multiple ruby problem"},
+	{url: "/en/blog/2014/02/25/jenkins-home-win/", title: "Jenkins: How to change JENKINS_HOME on Windows"},
+	{url: "/en/blog/2014/01/29/chef-win-path/", title: "Chef Console: avoid multiple ruby problem on Windows"},
 	{url: "/en/blog/2013/11/07/k-means/", title: "Visualizing K-Means algorithm with D3.js"},
 	{url: "/en/blog/2013/10/10/grunt-watch-slow/", title: "When grunt-contrib-watch uses too much CPU, try grunt-este-watch"},
-	{url: "/en/blog/2013/10/02/node-watch-impl/", title: "Difference between fs.watch() and fs.watchFile()"},
-	{url: "/en/blog/2013/07/09/symlink-dir-diff-on-windows/", title: "Use symlinks in git difftool --dir-diff on Windows"}
+	{url: "/en/blog/2013/10/02/node-watch-impl/", title: "Difference between fs.watch() and fs.watchFile()"}
 ];
 
 Site.archives.months = [
-  "201401"
+  "201402"
+, "201401"
 , "201311"
 , "201310"
 , "201307"
