097fc8b52cb8c0da4acb8e805d2aed3e1b2ad2e7
nitoyon <nitoyon@gmail.com>
2014-06-30 02:06:23 +0900

    Update for 2014-06-30

--- ja/posts.js
+++ ja/posts.js
@@ -12,15 +12,16 @@ var Site = {
 
 
 Site.archives.posts = [
+	{url: "/ja/blog/2014/06/30/vue-js-hook/", title: "Vue.js が data に渡した値を激しく書き換える件について"},
 	{url: "/ja/blog/2014/05/30/ruby-https-cert/", title: "Ruby で HTTPS 接続するときの証明書で悩んだ話"},
 	{url: "/ja/blog/2014/04/02/d3-event-dispatch/", title: "D3.js で自作クラスにイベント発行機能を追加する"},
 	{url: "/ja/blog/2014/03/28/git-crlf-to-lf/", title: "Git for Windows でレポジトリー上の CR LF を LF に変換する手順"},
-	{url: "/ja/blog/2014/03/14/utf8-str-count/", title: "全角半角混在の文章で 1 行に半角何文字分あるか調べる方法"},
-	{url: "/ja/blog/2014/03/07/fancy-git-bash/", title: "ConEmu 突っ込んだら Git for Windows の Git Bash がカッコよくなった"}
+	{url: "/ja/blog/2014/03/14/utf8-str-count/", title: "全角半角混在の文章で 1 行に半角何文字分あるか調べる方法"}
 ];
 
 Site.archives.months = [
-  "201405"
+  "201406"
+, "201405"
 , "201404"
 , "201403"
 , "201402"
