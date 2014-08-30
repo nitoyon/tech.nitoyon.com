b23ab32a64e611a54aa72d958514cc6a13e57a3f
nitoyon <nitoyon@gmail.com>
2014-08-30 22:57:01 +0900

    remove enum-lang-posts.html

--- _lang/posts.js
+++ _lang/posts.js
@@ -6,14 +6,13 @@ var Site = {
 	archives: {}
 };
 
-{% include enum-lang-posts.html %}
 Site.archives.posts = [
-{% for post in posts limit:5%}{% unless forloop.first %},
+{% for post in site.posts limit:5%}{% unless forloop.first %},
 {% endunless %}	{url: {{post.url | json}}, title: {{post.title | json}}}{% endfor %}
 ];
 
 Site.archives.months = [
-{% for post in posts %}{% capture this_year %}{{ post.date | date: "%Y" }}{% endcapture %}{% capture this_month %}{{ post.date | date: "%m" }}{% endcapture %}{% capture next_year %}{{ post.next.date | date: "%Y" }}{% endcapture %}{% capture next_month %}{{ post.next.date | date: "%m" }}{% endcapture %}{% if forloop.first %}  "{{this_year}}{{this_month}}"
+{% for post in site.posts %}{% capture this_year %}{{ post.date | date: "%Y" }}{% endcapture %}{% capture this_month %}{{ post.date | date: "%m" }}{% endcapture %}{% capture next_year %}{{ post.next.date | date: "%Y" }}{% endcapture %}{% capture next_month %}{{ post.next.date | date: "%m" }}{% endcapture %}{% if forloop.first %}  "{{this_year}}{{this_month}}"
 {% elsif forloop.last %}{% elsif this_year != next_year %}, "{{this_year}}{{this_month}}"
 {% elsif this_month != next_month %}, "{{this_year}}{{this_month}}"
 {% endif %}{% endfor %}];
