7914fce5793f4f82365c24b212539710ffd4a9d6
nitoyon <nitoyon@gmail.com>
2015-12-26 00:17:03 +0900

    Add format fix code

--- _plugins/ext/convertible_lf_fix.rb
+++ _plugins/ext/convertible_lf_fix.rb
@@ -10,8 +10,32 @@ module Jekyll
     def write(dest)
       path = destination(dest)
       FileUtils.mkdir_p(File.dirname(path))
+
+      # temporary fix for reduce diff between hparser and markdown
+      output = self.output
+        .gsub("\n\n", "\n")
+        .gsub(/<meta property="og:description" content="[^"]*"/, %q|<meta property="og:description" content="(snip)"|)
+        .gsub(/^<br \/>\n/, "")
+        .gsub("\t", "    ")
+        .gsub("<p><center>", "<center>")
+        .gsub("</center></p>", "</center>")
+        .gsub(%q|<div class="highlight"><pre>|, "<pre>")
+        .gsub("</pre></div>", "</pre>")
+        .gsub(/<pre><code[^>]+>/, "<pre>")
+        .gsub("</code></pre>", "</pre>")
+        .gsub("\n</pre>", "</pre>")
+        .gsub("</p><cite>", "</p><p><cite>")
+        .gsub("</cite></blockquote>", "</cite></p></blockquote>")
+        .gsub("<table><thead>", "<table>")
+        .gsub("</thead><tbody>", "")
+        .gsub("</tbody>", "")
+
+      # format
+      require 'htmlbeautifier'
+      output = HtmlBeautifier.beautify(output, tab_stops: 0)
+
       File.open(path, 'w') do |f|
-        f.write(self.output)
+        f.write(output)
       end
     end
   end
