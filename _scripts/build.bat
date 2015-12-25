87e9b1b2b648c2de82aa9e79bf3e661afb510c13
nitoyon <nitoyon@gmail.com>
2015-12-26 01:07:38 +0900

    Remove format_html.js
    now use htmlbeautifier

--- _scripts/build.bat
+++ _scripts/build.bat
@@ -8,6 +8,3 @@ ruby -I ..\..\hotchpotch\hparser\lib _scripts\convert_hatena_to_md.rb _posts\ja\
 
 echo jekyll
 call bundle exec grunt rebuild
-
-echo indent
-node _scripts\format_html.js _site\ja\blog\**\*.html
