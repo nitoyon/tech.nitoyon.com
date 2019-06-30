746b1a357c7a10101a2c47fea416448af89cac27
nitoyon <nitoyon@gmail.com>
2019-06-30 14:45:49 +0900

    Update docker-jekyll.bat (renamed from build.bat)

---
+++ _scripts/docker-jekyll.bat
@@ -0,0 +1,24 @@
+@echo off
+REM Execute jekyll with docker
+REM
+REM ex) build site
+REM   docker-jekyll.bat build --config _config.yml,_config.ja.yml
+REM
+REM ex) start bash
+REM   docker-jekyll.bat bash
+
+if /i %1 == bash (
+  set CMD=bash
+  echo Starting bash...
+) else (
+  set CMD=jekyll %*
+)
+
+
+docker run ^
+  --rm ^
+  --volume="%~dp0/../:/srv/jekyll" ^
+  --volume="%~dp0/../vendor/bundle:/usr/local/bundle" ^
+  -it ^
+  jekyll/jekyll:3.8 ^
+  %CMD%
--- build.bat
+++
@@ -1,8 +0,0 @@
-
-docker run ^
-  --rm ^
-  --volume="%~dp0:/srv/jekyll" ^
-  --volume="%~dp0/vendor/bundle:/usr/local/bundle" ^
-  -it ^
-  jekyll/jekyll:3.8 ^
-  jekyll build %*
