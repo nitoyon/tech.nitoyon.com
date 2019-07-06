9e6810d04d212afd5660af6e6c818703f55158fc
nitoyon <nitoyon@gmail.com>
2019-07-07 00:52:26 +0900

    post_link: Update to Jekyll 3.8.5

--- _plugins/tags/post_link.rb
+++ _plugins/tags/post_link.rb
@@ -21,12 +21,6 @@
 #     {% post_link category/2012-01-02-article-name %}
 #     => <a href="/category/2012/01/02/article-name.html">Article Title</a>
 #
-# Embed with file name. (If there are multiple posts with same file name with
-# different category name, it is not undefined that which posts are selected.
-#
-#     {% post_link 2012-01-02-article-name %}
-#     => <a href="/category/2012/01/02/article-name.html">Article Title</a>
-#
 # Specify link text
 #
 #     {% post_link 2012-01-02-article-name, here %}
@@ -43,27 +37,8 @@
 # BSD License.
 
 require 'cgi'
-require 'jekyll/tags/post_url'
 
 module Jekyll
-
-  # PostCategoryComparer adds `categories` property to PostComparer
-  # (defined in lib/jekyll/tags/post_url.rb).
-  # Note that Post#<=>() only checks `YYYY-MM-DD-slug` are equal
-  class PostCategoryComparer
-    MATCHER = /^(.+\/)*(\d+-\d+-\d+)-(.*)$/
-
-    attr_accessor :date, :slug, :categories
-
-    def initialize(name)
-      m, dir, date, slug = *name.match(MATCHER)
-      dir ||= ""
-      @categories = dir.split('/').reject { |x| x.empty? }
-      @slug = slug
-      @date = Time.parse(date)
-    end
-  end
-
   class PostLinkTag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
       super
@@ -85,26 +60,17 @@ module Jekyll
         raise "no param and post is not defined" unless context['post']
         url = context['post']
       end
-
-      begin
-        target_post = PostCategoryComparer.new(url)
-      rescue
-        target_post = nil
-      end
+      url = '/' + url unless url.start_with? '/'
 
       # Find post or page
       post = nil
       (site.posts.docs + site.pages).each do |p|
-        if p.url == url
+        post_url = p.url
+        post_url = p.cleaned_relative_path if p.class == Document
+        if post_url == url
           # stop enumerating if url exactly matches
           post = p
           break
-        elsif p == target_post
-          post = p
-          # stop enumerating if categories are specified and equal
-          if p.categories == target_post.categories
-            break
-          end
         end
       end
 
